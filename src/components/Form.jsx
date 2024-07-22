ab6626c6a5734a91558c4ac25cc4a6ba8d06dbd8
kuba-bednarczyk <kubabednarczyk2003@gmail.com>
2024-07-22 14:03:59 +0200

    making form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,15 +3,32 @@ const Form = () => {
   return (
     <div className='form'>
       <form>
-        <h3>General Info:</h3>
-        <label>Name: </label><input type="text" />
-        <label>Surname: </label><input type="text" /><br />
-        <label>Phone:</label><input type="text" />
-        <label>e-mail</label><input type="text" />
+        <div>
+          <h3>General Info:</h3>
+          <label>Name: </label><input type="text" />
+          <label>Surname: </label><input type="text" />
+          <label>Phone: </label><input type="text" />
+          <label>e-mail: </label><input type="text" />
+        </div>
 
+      <div>
         <h3>Education:</h3>
+        <label>Name of School: </label><input type="text" />
+        <label>Major: </label><input type="text" />
+        <label>Degree: </label>
+        <select>
+          <option value=""></option>
+          <option value=""></option>
+          <option value=""></option>
+          <option value=""></option>
+        </select>
+        <label>From: </label><input type="text" />
+        <label>To: </label><input type="text" />
+      </div>
         
+      <div>
         <h3>Job Experience:</h3>
+      </div>
 
       </form>
     </div>
